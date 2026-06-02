72880d8a0edd3d57e792bcb51b42c10c5c3335f6
lynaa08 <linakouache5@gmail.com>
2026-06-02 02:20:27 +0100

    Update database.js

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const Database = require('better-sqlite3');
 const path = require('path');
-
-const db = new Database(path.join(__dirname, 'mustech.db'));
+const dbPath = process.env.DATABASE_PATH || path.join('/tmp', 'mustech.db');
+const db = new Database(dbPath);
 
 // Enable WAL mode for better performance
 db.pragma('journal_mode = WAL');
